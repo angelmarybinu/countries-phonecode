95cf9f4abe5dc6749d83197e41082932a5b0a8b2
angelmarybinu <98399507+angelmarybinu@users.noreply.github.com>
2023-06-17 13:33:30 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,306 @@
-import Image from 'next/image'
+{/*import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import React from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({jsonData}) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+    <h1>Welcome</h1>
+    <h2>welcome</h2>
     </main>
   )
+}*/}
+// pages/index.js
+{/*import styles from './styles.module.css';
+
+import countriesData from '../public/data.json';
+import React, { useState } from 'react';
+const { countries } = countriesData;
+export default function Home({ countriesData }) {
+  
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5; 
+  const pageCount = Math.ceil(countries.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  // Filter and display items based on the current page
+  const displayedCountries = countries.slice(startIndex, endIndex);
+
+  // Handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const [searchText, setSearchText] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    // Filter countries based on search text
+    const filteredCountries = countries.filter((country) =>
+      country.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    setSearchResult(filteredCountries);
+  };
+
+  
+return (
+<main className={styles.main}>
+<div className={styles.sidebar}>
+      <form onSubmit={handleSearch}>
+        <input className={styles.search}
+          type="text"
+          placeholder="Search..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <ul>
+        {searchResult.map((country) => (
+          <li key={country.id}>
+            <div className={styles.box}>
+           {/*{country.name} - {country.phoneCode}*/}
+           {/*<span className={styles.countryName}>{country.name}</span> -{' '}
+           <span className={styles.phoneCode}>{country.phoneCode}</span>
+            </div>
+          </li>
+        ))}
+        </ul>
+    </div>
+  
+  <div className={styles.maincont}>
+    <h1 className={styles.heading}>Countries and Phone Codes</h1>
+    <ul>
+      {countries.map((country) => (
+        <li key={country.id}>
+          <div className={styles.box}>
+          <span className={styles.countryName}>{country.name}</span> - <span className={styles.phoneCode}>{country.phoneCode}</span>
+          </div>
+        </li>
+      ))}
+    </ul>
+    <div className={styles.pagination}>
+          {Array.from(Array(pageCount).keys()).map((index) => (
+            <button
+              key={index}
+              className={currentPage === index + 1 ? styles.active : ''}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+      </div>
+  
+
+</main>
+ 
+);
+      }
+    */}
+    {/*import styles from './styles.module.css';
+    import React, { useState } from 'react';
+    import countriesData from '../public/data.json';
+    
+    export default function Home() {
+      const { countries } = countriesData;
+      const itemsPerPage = 7;
+      const [currentPage, setCurrentPage] = useState(1);
+      const [searchText, setSearchText] = useState('');
+      const [searchResult, setSearchResult] = useState([]);
+
+      const handleSearch = (e) => {
+        e.preventDefault();
+    
+        // Filter countries based on search text
+        const filteredCountries = countries.filter((country) =>
+          country.name.toLowerCase().includes(searchText.toLowerCase())
+        );
+    
+        setSearchResult(filteredCountries);
+      };
+    
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      const countriesToShow = countries.slice(startIndex, endIndex);
+    
+      const handlePrevPage = () => {
+        if (currentPage > 1) {
+          setCurrentPage(currentPage - 1);
+        }
+      };
+    
+      const handleNextPage = () => {
+        const maxPage = Math.ceil(countries.length / itemsPerPage);
+        if (currentPage < maxPage) {
+          setCurrentPage(currentPage + 1);
+        }
+      };
+    
+      return (
+        <main className={styles.main}>
+          <div className={styles.sidebar}>
+            <input
+              className={styles.search}
+              type="text"
+              placeholder="Search..."
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <button type="submit">Search</button>
+    
+            <ul>
+              {searchResult.map((country) => (
+                <li key={country.id}>
+                  <div className={styles.box}>
+                    <span className={styles.countryName}>{country.name}</span> -{' '}
+                    <span className={styles.phoneCode}>{country.phoneCode}</span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+    
+          <div className={styles.maincont}>
+            <h1 className={styles.heading}>Countries and Phone Codes</h1>
+            <ul>
+              {countriesToShow.map((country) => (
+                <li key={country.id}>
+                  <div className={styles.box}>
+                    <span className={styles.countryName}>{country.name}</span> -{' '}
+                    <span className={styles.phoneCode}>{country.phoneCode}</span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+    
+            <div className={styles.pagination}>
+              <button
+                className={styles.prevButton}
+                onClick={handlePrevPage}
+                disabled={currentPage === 1}
+              >
+                &lt; Previous
+              </button>
+              <button
+                className={styles.nextButton}
+                onClick={handleNextPage}
+                disabled={endIndex >= countries.length}
+              >
+                Next &gt;
+              </button>
+            </div>
+          </div>
+        </main>
+      );
+    }*/}
+    import styles from './styles.module.css';
+import React, { useState } from 'react';
+import countriesData from '../public/data.json';
+
+export default function Home() {
+  const { countries } = countriesData;
+  const itemsPerPage = 7;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchText, setSearchText] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    // Filter countries based on search text
+    const filteredCountries = countries.filter((country) =>
+      country.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    setSearchResult(filteredCountries);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const countriesToShow = countries.slice(startIndex, endIndex);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    const maxPage = Math.ceil(countries.length / itemsPerPage);
+    if (currentPage < maxPage) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.sidebar}>
+        <form onSubmit={handleSearch}>
+          <input
+            className={styles.search}
+            type="text"
+            placeholder="Search..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        <ul>
+          {searchResult.length > 0 ? (
+            searchResult.map((country) => (
+              <li key={country.id}>
+                <div className={styles.box}>
+                  <span className={styles.countryName}>{country.name}</span> -{' '}
+                  <span className={styles.phoneCode}>{country.phoneCode}</span>
+                </div>
+              </li>
+            ))
+          ) : (
+            <li>No search results found.</li>
+          )}
+        </ul>
+      </div>
+
+      <div className={styles.maincont}>
+        <h1 className={styles.heading}>Countries and Phone Codes</h1>
+        <ul>
+          {countriesToShow.map((country) => (
+            <li key={country.id}>
+              <div className={styles.box}>
+                <span className={styles.countryName}>{country.name}</span> -{' '}
+                <span className={styles.phoneCode}>{country.phoneCode}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.pagination}>
+          <button
+            className={styles.prevButton}
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}
+          >
+            &lt; Previous
+          </button>
+          <button
+            className={styles.nextButton}
+            onClick={handleNextPage}
+            disabled={endIndex >= countries.length}
+          >
+            Next &gt;
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
+
+    
